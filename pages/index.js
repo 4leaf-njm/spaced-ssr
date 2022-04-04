e2d4816a025d4b19b11eba8064e993714a23400f
4leaf-njm <4leaf.njm@gmail.com>
2022-04-04 13:26:02 +0900

    modify

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { withApollo } from "../lib/apollo";
 import MM00 from "../src/Components/Routes/Client/MM00";
 import ClientLayout from "../src/Components/Routes/Layouts/ClientLayout";
@@ -6,8 +6,40 @@ import { NextSeo } from "next-seo";
 import seoConfig from "../src/seo.config.json";
 
 const Main = () => {
+  const wholeRef = useRef();
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (wholeRef.current) {
+        let html = "";
+
+        let s = document.createElement("script");
+        s.type = "text/javascript";
+        s.async = true;
+        s.src = "https://www.googletagmanager.com/gtag/js?id=G-JJPMTEZ9PN";
+
+        wholeRef.current.appendChild(s);
+
+        s = document.createElement("script");
+        s.type = "text/javascript";
+        s.async = true;
+
+        html = `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-JJPMTEZ9PN');
+        `;
+        s.innerHTML = html;
+
+        wholeRef.current.appendChild(s);
+      }
+    }, 100);
+  }, [wholeRef.current]);
+
   return (
-    <ClientLayout title={`메인 | ${process.env.HOMEPAGE_NAME}`}>
+    <ClientLayout ref={wholeRef} title={`메인 | ${process.env.HOMEPAGE_NAME}`}>
       <NextSeo
         title={seoConfig.title}
         subject={seoConfig.subject}
